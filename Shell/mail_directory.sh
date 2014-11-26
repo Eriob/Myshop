b8c7d91e606fab6d9827b76a396c0099fe0acf29
Eriob <hallay@intechinfo.fr>
2014-11-26 11:00:08 +0100

    Commit de scripts shell

---
+++ Shell/mail_directory.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+#------------------------------------------------------------
+# Auteur : Alababa
+# Nom : Script automatisation création mail directory + envoi mail
+# Role : Automatiser la création d'une boite mail
+#------------------------------------------------------------
+
+cd /var/mail
+
+function create_MailDirectory() {
+
+#On prendra en entrée :
+#$1 : le nom de l'utilisateur
+
+
+		if [[ test -e $1 ]]; then
+			echo "Ce repertoire existe déjà"
+		else
+			mkdir $1
+			cd ./$1
+			maildirmake -f ./Maildir
+		fi
+
+}
+
+function send_FirstMail() {
+
+echo "Bienvenue sur MySHOP, vous pouvez des maintenant vous connectez sur http://myshop.itinet.fr et créer votre boutique en ligne en quelques minutes. 
+L'équipe MySHOP (Ne pas répondre)" | mailx -s "Bienvenue sur MySHOP" $1@myshop.itinet.fr
+
+}
