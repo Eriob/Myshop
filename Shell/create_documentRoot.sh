b469c3ad6667331ba1702ba75de06331c9249251
Eriob <hallay@intechinfo.fr>
2014-11-26 15:13:00 +0100

    Commit après itération 2
    A la demande d'Alan

--- Shell/create_documentRoot.sh
+++ Shell/create_documentRoot.sh
@@ -6,19 +6,27 @@
 # Role : Automatiser la creation de virtualhosts
 #------------------------------------------------------------
 
-#On prendra en entrée
-#$1 : le nom de Document root de l'utilisateur
-
 
 function create_documentRoot() {
 
 #On prendra en entrée :
 #$1 : le nom du Document root de l'utilisateur 
 	
-	cd /var/sftp/myshop
-	$nom = $1
-
-	mkdir $nom
+	echo "STEP 1 : On crée le répertoire /var/sftp/$1"
+	mkdir /var/sftp/$1
+	echo "STEP 1 : Création [OK]"
+	echo "STEP 2 : On crée le virtualhost associé dans /etc/apache2/sites-available/$1"
+	echo "<VirtualHost *:80>
+		ServerName $1.myshop.itinet.fr
+		DocumentRoot /var/sftp/myshop/$1
+		Errorlog /var/log/apache2/$1.myshop.itinet.fr-error_log
+		CustomLog /var/log/apache2/$1.myshop.itinet.fr-access_log
+	</VirtualHost>
+	" > /etc/apache2/sites-available/$1
+	echo "STEP 2 : Création [OK]"
+	echo "STEP 3 : on crée un lien symbolique vers sites-enabled"
+	ln -s /etc/apache2/sites-available/$1 /etc/apache2/sites-enabled/
+	echo "STEP 3 : Création [OK]"
 
 }
 
@@ -32,33 +40,19 @@ function manage_documentRoot() {
 
 #Pour supprimer son document Root
 #$2 : le nom de document Root a supprimer
-	cd /var/sftp/myshop
 
 	if [[ $1 = 1 ]]; then
 		# On renomme $2 en $3 ($2 = ancien nom / $3 = nouveau nom)
-		mv $2 $3
+		echo "On renome $2 en $3"
+		mv /var/sftp/$2 /var/sftp/$3
+		echo "Renommage [OK]"
 	elif [[ $1 = 2 ]]; then
 		# On desactive le document root $2 ($2 = nom du repertoire)
+		echo "On retire le lien symbolique de /etc/apache2/sites-available/$2"
 		unlink /etc/apache2/sites-available/$2
+		echo "Suppression [OK]"
 	else
-		echo "Cette action n'est pas possible"
+		echo "Cette action n'est pas possible, veuillez checker votre script :)"
 	fi
-
-}
-
-
-function create_virtualhost() {
-
-	cd /etc/apache2/
-
-	echo "<VirtualHost *:80>
-		ServerName $1.myshop.itinet.fr
-		DocumentRoot /var/sftp/myshop/$1
-		Errorlog /var/log/apache2/$1.myshop.itinet.fr-error_log
-		CustomLog /var/log/apache2/$1.myshop.itinet.fr-access_log
-	</VirtualHost>
-	" > sites-available/$1
-
-	ln -s sites-available/$1 sites-enabled/
-
+	
 }
