262b383d199d3e55fb668accf57335e6e36a771f
Alan <alan.barthelemy@gmail.com>
2014-12-01 13:36:37 +0100

    modif

--- Shell/manage_users.sh
+++ Shell/manage_users.sh
@@ -19,11 +19,14 @@ $2 = mkpasswd -H md5 $2
 	useradd $1 --gid 5001 --groups 5001 --home /var/sftp/$1 --shell /bin/MySecureShell
 	echo "Création [OK]"
 
-	if [[ !test -d /var/sftp/$1 ]]; then
+	if [[ test -d /var/sftp/$1 ]]; then
+		echo "Répertoire /Var/sftp/$1 déja crée [OK]"
+	else
 		echo "Création du répertoire /var/sftp/$1/www en cours..."
 		mkdir /var/sftp/$1/www
 		echo "Création [OK]"
 	fi
+
 
 	echo "Modification des droits sur le dossier utilisateur /var/sftp/$1"
 	chown -R $1:sftp /var/sftp/$1
