03dff77be7f5739c7956bead22d51366764a180d
Alan <alan.barthelemy@gmail.com>
2014-12-19 16:25:06 +0100

    Model subscribe

--- www/Model/Msubscribe.php
+++ www/Model/Msubscribe.php
@@ -6,7 +6,7 @@ $date = "0";
 
 /*ID, FIRSTNAME, LASTNAME, PSEUDO, PASSWORD, EMAIL, SHOP, ADRESSE, DATE INSCRIPTION, PHONE, AVATAR, BAN, ADMIN, TEAM, STATUS, ZIP_CODE*/
 
-$sql = 'INSERT INTO users VALUES("", "'.$firstname.'", "'.$lastname.'", "'.$pseudo.'","'.$mdp.'","'.$email.'", "'.$name.'","'.$date.'", "'.$telephone.'", "", "0", "0", "", "", "")';
+$sql = 'INSERT INTO users VALUES("", "'.$firstname.'", "'.$lastname.'", "'.$pseudo.'","'.$mdp.'","'.$email.'", "'.$name.'", "", "'.$date.'", "'.$telephone.'", "", "0", "0", "", "", "")';
 	
 $request = mysql_query($sql) or die (mysql_error());
 
