94d6541d6da8b0885e431b87d32505356d45d4d1
Alan <alan.barthelemy@gmail.com>
2014-12-20 19:08:49 +0100

    mail web

--- www/Server/add_mailDirectory.sh
+++ www/Server/add_mailDirectory.sh
@@ -15,18 +15,18 @@
 if [[ -d "/var/mail/$1" ]]; then
 	/bin/echo "Ce repertoire existe déjà [ECHEC]"
 else
-	/bin/mkdir /var/mail/$1
+	sudo /bin/mkdir /var/mail/$1
 
 	#PAS BESOIN CAR ON ENVOI UN MAIL
-	/usr/bin/maildirmake -f ./Maildir
+	sudo /usr/bin/maildirmake -f ./Maildir
 
-	/bin/chown -R vmail.vmail /var/mail/$1
+	sudo /bin/chown -R vmail.vmail /var/mail/$1
 
 	/bin/echo "$1@myshop.itinet.fr $1/Maildir/" >> /etc/postfix/vmailbox
-	/usr/sbin/postmap /etc/postfix/vmailbox
-	/usr/sbin/userdb $1@myshop.itinet.fr set uid=5000 gid=5000 home=/var/mail/$1 mail=/var/mail/$1/Maildir
+	sudo /usr/sbin/postmap /etc/postfix/vmailbox
+	sudo /usr/sbin/userdb $1@myshop.itinet.fr set uid=5000 gid=5000 home=/var/mail/$1 mail=/var/mail/$1/Maildir
 	/bin/echo "$2" | /usr/sbin/userdbpw -md5 | /usr/sbin/userdb $1@myshop.itinet.fr set systempw
-	/usr/sbin/makeuserdb
+	sudo /usr/sbin/makeuserdb
 
 	/bin/echo "On envoi un mail de bienvenue"
 	#echo "Bienvenue sur MySHOP, vous pouvez des maintenant vous connectez sur http://myshop.itinet.fr et créer votre boutique en ligne en quelques minutes. 
--- www/Server/add_webUser.sh
+++ www/Server/add_webUser.sh
@@ -13,23 +13,23 @@
 #$2 : le mot de passe de l'utilisateur
 #$3 : le nom de la boutique
 
-if /bin/grep "^$1:" /etc/passwd ; then
+if /bin/grep "^$1:" /etc/passwd; then
 	/bin/echo "Cet utilisateur UNIX existe déjà ! [ECHEC]"
 else
-	/usr/sbin/useradd --home /var/sftp/$1 --gid 5001 --groups 5001 --password "$(mkpasswd "$2")" --shell /bin/MySecureShell $1
+	sudo /usr/sbin/useradd --home /var/sftp/$1 --gid 5001 --groups 5001 --password "$(mkpasswd "$2")" --shell /bin/MySecureShell $1
 		
-	if [[ -d "/var/sftp/$1" ]]; then
+	if test -d "/var/sftp/$1"; then
 		/bin/echo "Répertoire /var/sftp/$1 déja crée [OK]"
 	else
-		/bin/mkdir /var/sftp/$1/
-		/bin/mkdir /var/sftp/$1/www 
+		sudo /bin/mkdir /var/sftp/$1/
+		sudo /bin/mkdir /var/sftp/$1/www 
 
-		/bin/chown -R $1:sftp /var/sftp/$1
+		sudo /bin/chown -R $1:sftp /var/sftp/$1
 
-		/usr/sbin/edquota -p test $1
+		sudo /usr/sbin/edquota -p test $1
 	fi
 
-	if [[ -d "/etc/apache2/sites-available/$3" ]]; then
+	if test -d "/etc/apache2/sites-available/$3"; then
 		/bin/echo "Virtualhost déjà crée [OK]"
 	else
 		/bin/echo "<VirtualHost *:80>
@@ -37,7 +37,7 @@ else
 				DocumentRoot /var/sftp/$1/www
 				Errorlog /var/log/apache2/$3.myshop.itinet.fr-error_log
 				</VirtualHost>" > /etc/apache2/sites-available/$3
-		/bin/ln -s /etc/apache2/sites-available/$3 /etc/apache2/sites-enabled/
+		sudo /bin/ln -s /etc/apache2/sites-available/$3 /etc/apache2/sites-enabled/
 	fi
 fi
 
