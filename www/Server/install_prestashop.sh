2511cad277bcbb8638a91f5d9e1329ec973fa724
Alan <alan.barthelemy@gmail.com>
2014-12-22 21:18:00 +0100

    install_prestashop

--- www/Server/install_prestashop.sh
+++ www/Server/install_prestashop.sh
@@ -13,7 +13,7 @@
 #$2 : le mot de passe de l'utilisateur
 #$3 : l'adresse email
 
-	/usr/bin/mysql -u root -padmin -Bse "CREATE DATABASE $1;CREATE USER $1@localhost;SET PASSWORD FOR $1@localhost = PASSWORD('$2')"
+	/usr/bin/mysql -u root -padmin -Bse "CREATE DATABASE $1;CREATE USER $1@localhost;SET PASSWORD FOR $1@localhost = PASSWORD('$2');GRANT ALL PRIVILEGES ON $1.* TO $1@localhost IDENTIFIED BY '$2';"
 	
 	#COPIER L'INTEGRALITE DE PRESTASHOP
 	sudo /bin/cp -r /var/sftp/myshop/prestashop/* /var/sftp/$1/www/
@@ -42,7 +42,6 @@
 
 	sudo /bin/chown -R www-data.www-data /var/sftp/$1/www/*
 	sudo /bin/chmod -R 777 /var/sftp/$1/www/*
-	sudo /bin/echo $1 $2 $3
 	sudo /usr/bin/php /var/sftp/$1/www/install/index_cli.php --domain=$1.myshop.itinet.fr --db_name=$1 --db_user=$1 --db_password=$2 --password=$2
 	sudo /bin/mv /var/sftp/$1/www/admin /var/sftp/$1/www/admin5100
 	#sudo /bin/rm -r /var/sftp/$1/www/install
