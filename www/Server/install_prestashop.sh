7c1e7e1f0172ce96940589254af44b2b39b127e7
Alan <alan.barthelemy@gmail.com>
2014-12-22 11:44:55 +0100

    in

--- www/Server/install_prestashop.sh
+++ www/Server/install_prestashop.sh
@@ -13,7 +13,7 @@
 #$2 : le mot de passe de l'utilisateur
 #$3 : l'adresse email
 
-	sudo /bin/cp /var/sftp/myshop/prestashop/* /var/sftp/$1/www/
+	sudo /bin/cp -r /var/sftp/myshop/prestashop/* /var/sftp/$1/www/
 	php /var/sftp/$1/www/install/index_cli.php --domain=$1.myshop.itinet.fr --db_name=$1 --db_user=$1 --db_password=$2 --email=$3 --password=$2
 	sudo /bin/mv /var/sftp/$1/www/admin admin5100
 	sudo /bin/rm -r /var/sftp/$1/www/install
