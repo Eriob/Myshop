11d9fe287c130bdb4d04a237bdf321c000948d2e
Alan <alan.barthelemy@gmail.com>
2014-12-22 15:05:00 +0100

    msub

--- www/Model/Msubscribe.php
+++ www/Model/Msubscribe.php
@@ -30,12 +30,12 @@ or die ('ERROR TO CONNECT WITH DATABASE');
 $create_db = "CREATE DATABASE ".$name;
 $create_user = "CREATE USER '".$name."'@localhost";
 $create_pwd = "SET password FOR '".$name."'@localhost = password('".$password."'')";
-//$create_privilege = "GRANT ALL ON '".$name."'.* TO '".$name."'@localhost";
+$create_privilege = "GRANT ALL ON '".$name."'.* TO '".$name."'@localhost";
 
-$request_db = mysql_query($create_db) or die(mysql_error());
-$request_user = mysql_query($create_user) or die(mysql_error());
-$request_pwd = mysql_query($created_pwd) or die(mysql_error());
-//$request_privilege = mysql_query($create_privilege) or die(mysql_error());
+$request_db = mysql_query($create_db);
+$request_user = mysql_query($create_user);
+$request_pwd = mysql_query($created_pwd);
+$request_privilege = mysql_query($create_privilege);
 
 }
 
