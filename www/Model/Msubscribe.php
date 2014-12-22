96f728931d4faba821f0d86da0a2b832bcc02d03
Alan <alan.barthelemy@gmail.com>
2014-12-22 14:27:39 +0100

    msub vierge

--- www/Model/Msubscribe.php
+++ www/Model/Msubscribe.php
@@ -26,12 +26,12 @@ function install_prestashop($name, $password) {
 mysql_connect('localhost','root', 'admin')
 or die ('ERROR TO CONNECT WITH DATABASE');
 
-$create_db = "CREATE DATABASE '".$name."'";
+//$create_db = "CREATE DATABASE '".$name."'";
 //$create_user = 'CREATE USER "'.$name.'"@localhost';
 //$create_pwd = 'SET password FOR "'.$name.'"@localhost = password("'.$password.'")';
 //$create_privilege = 'GRANT ALL ON "'.$name.'".* TO "'.$name.'"@localhost';
 
-$request_db = mysql_query($create_db) or die(mysql_error());
+//$request_db = mysql_query($create_db) or die(mysql_error());
 //$request_user = mysql_query($create_user) or die(mysql_error());
 //$request_pwd = mysql_query($created_pwd) or die(mysql_error());
 //$request_privilege = mysql_query($create_privilege) or die(mysql_error());
