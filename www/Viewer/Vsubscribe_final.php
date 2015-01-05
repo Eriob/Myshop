4a78b8d5ae06f9ddab7268f11e499ee7c6c49d08
Alan <alan.barthelemy@gmail.com>
2015-01-05 17:10:42 +0100

    Vsubscribe_final

--- www/Viewer/Vsubscribe_final.php
+++ www/Viewer/Vsubscribe_final.php
@@ -3,11 +3,12 @@
     <div class="row">
         <br>
         <br>
+        <div class="col-md-12 mt32 mb16">
         <h4> Votre site <?php echo $_POST['name'];?> a été créé.
         <h5> Vous allez reçevoir un mail à <?php echo $_POST['email'];?>
         <br> Vous pouvez dès à présent vous connecter sur votre espace via filezilla (par exemple) avec vos identifiants.
         <br> Votre site est disponible à l'adresse suivante : <a href="<?php echo $_POST['name']."myshop.itinet.fr";?>">
-                    
+        </div>            
         
         <hr class="featurette-divider">
 
