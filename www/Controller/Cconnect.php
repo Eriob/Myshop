165988ff0be905ac8c0b605796a2f4a2bac34216
Eriob <hallay@intechinfo.fr>
2015-01-07 11:06:05 +0100

    Mmodif

--- www/Controller/Cconnect.php
+++ www/Controller/Cconnect.php
@@ -3,16 +3,21 @@
 
 	// On vérifie si la combinaison pseudo/pwd est correct
 	if (isset($_POST['pseudo'],$_POST['password']))
-	{		
-			$connect = connect($_POST['pseudo'],$_POST['password'])
+	{		$password = md5($_POST['password']);
+			$connect = connect($_POST['pseudo'],$password)
 	
-	if ($_POST['password'] == $connect['password'])
+	if ($password == $connect['password'])
 	{
 		$_SESSION['pseudo'] = $connect['pseudo'];
 		$_SESSION['id'] = $connect['id'];
 			
 			sleep(1);
 			header('location: index.php');
+			
+	/*LIGNES DE TEST*/
+	echo $_SESSION['pseudo'];
+	echo $_SESSION['id'];
+	
 	}
 
 
