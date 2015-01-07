c5c07e120c56b32d353e146fb24dd4efbe0458b3
Alan <alan.barthelemy@gmail.com>
2015-01-07 14:32:23 +0100

    Cconnect

--- www/Controller/Cconnect.php
+++ www/Controller/Cconnect.php
@@ -1,30 +1,23 @@
 <?php
-	include ('./model/Mconnect.php');
+	include ('./Model/Mconnect.php');
 
 	// On vérifie si la combinaison pseudo/pwd est correct
-	if (isset($_POST['pseudo'],$_POST['password']))
-	{		
+	if (isset($_POST['pseudo'],$_POST['password'])) {		
 		$password = md5($_POST['password']);
 		$connect = connect($_POST['pseudo'],$password);
 	
-	if ($password == $connect['password'])
-	{
-		$_SESSION['pseudo'] = $connect['pseudo'];
-		$_SESSION['id'] = $connect['id'];
-			
-		sleep(1);
-		header('location: index.php');
-	
-	}
-
-
-	else
-	{
-		$msg = "Mauvais mot de passe";
-		sleep(1);
-		header('location: index.php');		
-	}
-	
+		if ($password == $connect['password']) {
+			$_SESSION['pseudo'] = $connect['pseudo'];
+			$_SESSION['id'] = $connect['id'];
+				
+			sleep(1);
+			header('location: index.php');
+		
+		}else{
+			$msg = "Mauvais mot de passe";
+			sleep(1);
+			header('location: index.php');		
+		}	
 	}
 
 ?>
