a22c8716315a32e9a7867a4b211367a04fb28c00
Alan <alan.barthelemy@gmail.com>
2015-01-07 17:40:42 +0100

    vsub2

--- www/Viewer/Vsubscribe_step2.php
+++ www/Viewer/Vsubscribe_step2.php
@@ -23,10 +23,11 @@
         <br> Votre site est disponible à l'adresse suivante : <a href="<?php echo $_POST['name'];?>" TARGET=_BLANK><?php echo $_POST['name']."myshop.itinet.fr";?></a>
         </div>            
         
+    <form class="form-horizontal" method="POST" action="index.php?index=valid_subscribe">
         <input type="hidden" name="pseudo" value="<?echo $_POST['pseudo'];?>">
         <input type="hidden" name="password" value="<?echo $_POST['password'];?>">
-        
         <a class="btn btn-default" href="index.php?index=valid_subscribe" role="submit"> Se connecter </a>
+    </form>
 
         <hr class="featurette-divider">
 
