f00d98e1896f89cf56d925b0d4260c4e41b6d110
Alan <alan.barthelemy@gmail.com>
2015-01-13 14:24:12 +0100

    Minfor

--- www/Model/Mget_informations.php
+++ www/Model/Mget_informations.php
@@ -4,7 +4,7 @@ function get_informations($id) {
 	$sql = "SELECT pseudo, email, shop FROM users WHERE id = '$id'";
 	$request = mysql_query($sql) or die(mysql_error());
 
-	$informations = mysql_fetch_array($request)
+	$informations = mysql_fetch_array($request);
 
 	return $informations;
 }
