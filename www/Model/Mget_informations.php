3a324a46fbcd8189379d55cc238a6554a51ea60a
Alan <alan.barthelemy@gmail.com>
2015-01-13 15:51:53 +0100

    Mget

--- www/Model/Mget_informations.php
+++ www/Model/Mget_informations.php
@@ -1,7 +1,7 @@
 <?php
 
-function get_informations($pseudo) {
-	$sql = "SELECT pseudo, email, shop FROM users WHERE pseudo = '$pseudo'";
+function get_informations($id) {
+	$sql = "SELECT pseudo, email, shop FROM users WHERE id = '$id'";
 	$request = mysql_query($sql) or die(mysql_error());
 
 	$informations = mysql_fetch_array($request);
