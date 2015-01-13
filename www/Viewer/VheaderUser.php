d6e85ba77f2e03364a7235022bc2d0f98c500b5c
Alan <alan.barthelemy@gmail.com>
2015-01-13 16:55:23 +0100

    VheaderUser

--- www/Viewer/VheaderUser.php
+++ www/Viewer/VheaderUser.php
@@ -66,7 +66,7 @@
                     <a data-toggle="dropdown" href="#"><?php echo $_SESSION['pseudo'];?><b class="caret"></b></a>
                     <ul class="dropdown-menu">
                       <li><a href="index.php?index=myProfil">Mon Profil</a></li>
-                      <li><a href="index.php?index=show_mywebsite">Mon Site</a></li>
+                      <li><a href="index.php?index=show_mywebsite" target="_blank">Mon Site</a></li>
                       <li><a href="index.php?index=get_informations">Mes informations</a></li>
                       <li><a href="index.php?index=myOptions">Mes options</a></li>
                       <li class="divider"></li>
