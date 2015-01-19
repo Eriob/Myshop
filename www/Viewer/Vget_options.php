fb914598ca2fd023c0e55b6d39f552894bff7459
Alan <alan.barthelemy@gmail.com>
2015-01-19 14:59:05 +0100

    voptions

--- www/Viewer/Vget_options.php
+++ www/Viewer/Vget_options.php
@@ -2,33 +2,23 @@
         <input type="hidden" name="pseudo" value="<?php echo $options['name'];?>">
 
 <script type="text/javascript">
-var c,c2, ch;
- 
-// ajouter un champ avec son "mail" propre;
-function plus(){
-c=document.getElementById('cadre');
-c2=c.getElementsByTagName('input');
-ch=document.createElement('input');
- 
-ch.setAttribute('type','text');
-ch.setAttribute('mail','ch'+c2.length);
-c.appendChild(ch);
- 
-document.getElementById('mail').style.display='inline';
-}
- 
-// supprimer le dernier champ;
-function moins(){
-if(c2.length>0){c.removeChild(c2[c2.length-1])}
-if(c2.length==0){document.getElementById('mail').style.display='none'};
-}
+    $(document).ready(function()
+    {
+        $('#addInput').click(function()
+        {
+            var c = $("#mail input:last").clone();
+  
+            var mail = $(c).attr('mail');
+            value = mail.split('-');
+            mail = value[0]+'-'+(parseInt(value[1])+1);
+ 
+            $(c).attr('mail', mail);
+            $("#mail").append(c);
+        });
+    });
 </script>
-
-<div id="cadre" style="margin-left:100px;width:200px">
-</div>
- 
 <p>
-<input type="submit" value="Ajouter un mail" onclick="plus()" />
+<input type="submit" id="addInput" value="Ajouter un mail" onclick="plus()" />
 <input type="submit" style="display:none" id="mail" value="Supprimer le dernier mail" onclick="moins()" />
 </p>
 
