1467d7366566f53734b1053a5b60228e1161e8d8
Alan <alan.barthelemy@gmail.com>
2015-01-19 13:43:56 +0100

    Mget_informations

--- www/Model/Mget_informations.php
+++ www/Model/Mget_informations.php
@@ -2,7 +2,7 @@
 
 function get_informations($name) {
 	
-	$sql = "SELECT name, email, shop FROM users WHERE name = '$name'";
+	$sql = "SELECT email, shop FROM users WHERE shop = '$name'";
 	$request = mysql_query($sql) or die(mysql_error());
 
 	$informations = mysql_fetch_array($request);
