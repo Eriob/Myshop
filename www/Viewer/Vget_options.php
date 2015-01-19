f75a8aded0c98586a09ee1ef437fbbf2d5ca79aa
Alan <alan.barthelemy@gmail.com>
2015-01-19 16:18:02 +0100

    voptions

--- www/Viewer/Vget_options.php
+++ www/Viewer/Vget_options.php
@@ -4,17 +4,19 @@
 <p>
 </p>
 <br>
-    <form class="form-horizontal" name="options_form" method="POST" action="index.php?index=get_options">
-        <div class="form-group">
-            <label for="addmail" class="control-label">Ajouter une adresse mail : </label> 
-            <input type="text" name="addmail" style="max-width=100" class="form-control" placeholder="Nouvelle adresse">
-            <span class="input-group-addon">@<?php echo $_SESSION['name'].".myshop.itinet.fr";?></span> 
-         </div>
-<br>
-        <button type="submit" href="index.php?index=get_options" class="btn btn-primary pull-left">Valider</button>
-        </form>
+
+<form class="form-inline" method="POST" name="options_form" action="index.php?index=get_options">
+    <div class="input-group">
+        <input type="text" class="form-control" style="text-align:right" name="addmail" placeholder="Nouvelle adresse mail">
+        <span class="input-group-addon">@<?php echo $_SESSION['name'].".myshop.itinet.fr";?></span> 
     </div>
+    
+    <div class="form-group">
+    <br>
+    <button type="submit" href="index.php?index=get_options" class="btn btn-primary pull-left">Valider</button>
+</form>
     </div>
+</div>
 </div>
 
 <div class="container marketing">
