d07ccfc11725bef5939b60096d1eaef1e984eedd
Alan <alan.barthelemy@gmail.com>
2015-01-19 14:40:25 +0100

    Voptions

--- www/Viewer/Vget_options.php
+++ www/Viewer/Vget_options.php
@@ -2,8 +2,6 @@
         <input type="hidden" name="pseudo" value="<?php echo $options['name'];?>">
         
         <br><br>
-        
-        <hr class="featurette-divider">
         <button type="submit" href="index.php?index=get_options" class="btn btn-primary pull-left">Ajouter des adresses mails</button>
         </form>
     </div>
