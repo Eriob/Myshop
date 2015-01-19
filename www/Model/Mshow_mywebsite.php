5618b6c48fa4d93b3106f90dad2d33ae6d5c51d3
Alan <alan.barthelemy@gmail.com>
2015-01-19 13:42:34 +0100

    show

--- www/Model/Mshow_mywebsite.php
+++ www/Model/Mshow_mywebsite.php
@@ -2,7 +2,7 @@
 
 function show_mywebsite($name) {
 
-	$sql = "SELECT shop FROM users WHERE name = '$name'";
+	$sql = "SELECT shop FROM users WHERE shop = '$name'";
 	$request = mysql_query($sql) or die(mysql_error());
 
 	$name = mysql_fetch_assoc($request);
