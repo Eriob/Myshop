983066c4d7e558a7ee014246c71a68550d1b89cb
Alan <alan.barthelemy@gmail.com>
2015-01-21 10:40:00 +0100

    Mdelete

--- www/Model/Mdelete_shop.php
+++ www/Model/Mdelete_shop.php
@@ -3,10 +3,10 @@
 
 function delete_shop($name) {
 
-$sql = "DELETE FROM users WHERE shop =".$name;
+$sql = "DELETE FROM users WHERE shop = '$name'";
 $request = mysql_query($sql) or die (mysql_error());
 
-$sql2 = "DROP DATABASE ".$name;
+$sql2 = "DROP DATABASE '$name'";
 $request2 = mysql_query($sql2) or die(mysql_error());
 
 
