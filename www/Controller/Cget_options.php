d4086f978981a431e367dfd83651a2363a119dc3
Alan <alan.barthelemy@gmail.com>
2015-01-29 11:37:29 +0100

    Cget_options

--- www/Controller/Cget_options.php
+++ www/Controller/Cget_options.php
@@ -9,11 +9,10 @@ if (isset($_POST['addmail'])) {
 	$_POST['addmail'] = $_POST['addmail']."@".$_SESSION['name'].".myshop.itinet.fr";
 	$mailBDD = add_mails($_SESSION['name'], $_POST['addmail']);
 	
-	$name = escapeshellcmd($_SESSION['name']);
 	$pseudo = escapeshellcmd($pseudo);
 	$password = escapeshellcmd($_POST['password']);
 
-	$exec_addmail = sprintf('/var/www/Myshop/www/Server/add_mailDirectoryPLUS.sh %s $s $s', $pseudo, $name, $password);
+	$exec_addmail = sprintf('/var/www/Myshop/www/Server/add_mailDirectoryPLUS.sh %s $s $s', $pseudo, $_SESSION['name'], $password);
 
 	// Execution des commande
 	exec($exec_addmail);
