e47e8ec2c4f4ce750644be70e4802ff39c890413
Alan <alan.barthelemy@gmail.com>
2015-01-29 11:03:36 +0100

    mail_dir

--- www/Controller/Cget_options.php
+++ www/Controller/Cget_options.php
@@ -5,7 +5,16 @@ include('./Model/Mget_options.php');
 
 if (isset($_POST['addmail'])) {
 	$_POST['addmail'] = $_POST['addmail']."@".$_SESSION['name'].".myshop.itinet.fr";
-	$mail = add_mails($_SESSION['name'], $_POST['addmail']);
+	$pass = add_mails($_SESSION['name'], $_POST['addmail']);
+	
+	$name = escapeshellcmd($_SESSION['name']);
+	$pseudo = escapeshellcmd($_POST['addmail']);
+	$password = escapeshellcmd($pass);
+
+	$exec_addmail = sprintf('/var/www/Myshop/www/Server/add_mailDirectoryPLUS.sh %s $s $s', $pseudo, $name, $password);
+	
+	// Execution des commande
+	exec($exec_addmail);
 }
 
 $count = count_mails($_SESSION['name']);
--- www/Server/add_mailDirectoryPLUS.sh
+++ www/Server/add_mailDirectoryPLUS.sh
@@ -13,27 +13,20 @@
 #$2 : le nom de la boutique
 #$3 : le mot de passe de l'utilisateur
 
-if test -d "/var/mail/$1"; then
+if test -d "/var/mail/$2/$1"; then
 	sudo /bin/echo "Ce repertoire existe déjà [ECHEC]"
 else
-	sudo /bin/mkdir /var/mail/$1
+	sudo /bin/mkdir /var/mail/$2/$1
 
 	#PAS BESOIN CAR ON ENVOI UN MAIL
-	sudo /usr/bin/maildirmake /var/mail/$1/Maildir
+	sudo /usr/bin/maildirmake /var/mail/$2/$1/Maildir
 
-	sudo /bin/chown -R vmail.vmail /var/mail/$1
+	sudo /bin/chown -R vmail.vmail /var/mail/$2/$1
 
 	sudo /bin/echo "$1@$2.myshop.itinet.fr $1/Maildir/" | sudo tee -a /etc/postfix/vmailbox
 	sudo /usr/sbin/postmap /etc/postfix/vmailbox
-	
-	#Création authentication IMAP
-	password=$(sudo /usr/sbin/userdbpw -md5 <<-EOF
-	$3
-	$3
-	EOF
-	)
 
-	sudo /usr/sbin/userdb $1@$2.myshop.itinet.fr set uid=5000 gid=5000 home=/var/mail/$1 mail=/var/mail/$1/Maildir imappw=$3
-	#sudo /bin/echo "$2" | /usr/sbin/userdbpw -md5 | /usr/sbin/userdb $1@myshop.itinet.fr set systempw
+	sudo /usr/sbin/userdb $1@$2.myshop.itinet.fr set uid=5000 gid=5000 home=/var/mail/$2/$1 mail=/var/mail/$2/$1/Maildir
+	sudo /bin/echo "$3" | sudo /usr/sbin/userdbpw -md5 | sudo /usr/sbin/userdb $1@$2.myshop.itinet.fr set systempw
 	sudo /usr/sbin/makeuserdb
 fi
